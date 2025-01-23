94c26308d3a9472182cf3efbd4422419e66f26d7
Bengel Adoma <bengelansah@gmail.com>
2025-01-23 14:49:21 +0100

    first issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Color from "./Components/Color/Color";
 import "./App.css";
 
 function App() {
+  console.log("Find Issue 1");
   return (
     <>
       <h1>Theme Creator</h1>
 
       {initialColors.map((color) => {
         return <Color key={color.id} color={color} />;
+       
       })}
     </>
   );
