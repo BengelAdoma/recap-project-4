cda3e84b33b7037ab5095f26161f31353f9f58b2
Bengel Adoma <bengelansah@gmail.com>
2025-01-24 11:28:29 +0100

    issue resolved

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -12,7 +12,7 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
-       console.log("Find Issue 1");
+       
     </div>
     
   );
